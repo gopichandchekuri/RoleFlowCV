11e3d35530c7798cc1019180108e466b04763882
Gopichand Chekuri <gopichandchekuri@Gopichands-MacBook-Air.local>
2026-05-02 01:48:37 +0530

    fix: use 0.0.0.0 in production for Railway

--- server/index.ts
+++ server/index.ts
@@ -85,8 +85,8 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 3000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || "3000", 10);
-  httpServer.listen(port, "127.0.0.1", () => {
+  const host = process.env.NODE_ENV === "production" ? "0.0.0.0" : "127.0.0.1";
+  httpServer.listen(port, host, () => {
     log(`serving on port ${port}`);
   });
 })();
